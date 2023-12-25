2316ce3579fadbe86378321bd090ea49f62acef3
TrekkieCoder <111065900+TrekkieCoder@users.noreply.github.com>
2023-12-26 00:38:20 +0900

    Update tcp_tuner.sh

--- tcp_tuner.sh
+++ tcp_tuner.sh
@@ -13,6 +13,7 @@ sysctl net.ipv4.tcp_window_scaling >> sysctl_orig.txt
 sysctl net.ipv4.tcp_orphan_retries >> sysctl_orig.txt
 sysctl net.ipv4.tcp_sack >> sysctl_orig.txt
 sysctl net.ipv4.tcp_fin_timeout >> sysctl_orig.txt
+sysctl net.core.optmem_max >> sysctl_orig.txt
 
 echo "
 net.ipv4.tcp_keepalive_time=30
@@ -28,6 +29,7 @@ net.core.wmem_default=16777216
 net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_orphan_retries=0
 net.ipv4.tcp_sack=0
+net.core.optmem_max=1020000
 net.ipv4.tcp_fin_timeout=10" >> /etc/sysctl.conf
 
 sysctl -f
